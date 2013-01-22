896c5cf73deb329fe2e55352a8971f609393d550
Delisa Mason <iskanamagus@gmail.com>
2013-01-21 20:10:28 -0500

    Simplify constants

--- lib/bangou.rb
+++ lib/bangou.rb
@@ -19,86 +19,86 @@ class Bangou
 
   private
 
-  NUMERALS = {
-		0 => "",   1 => "一", 2 => "二",
-		3 => "三", 4 => "四", 5 => "五",
-		6 => "六", 7 => "七", 8 => "八",
-		9 => "九"
-	}
+  NUMERAL_INDEX = 0
+  TEXT_INDEX    = 1
+  RANGE_ERROR_MESSAGE = "Bangou can only process numbers between 0 and 99,999,999"
 
-	NUMERALS_TEXT = {
-		0 => "",    1 => "いち", 2 => "に",
-		3 => "さん", 4 => "よん", 5 => "ご",
-		6 => "ろく", 7 => "なな", 8 => "はち",
-		9 => "きゅう"
+  NUMERALS = {
+		0 => ["",""],      1 => ["一","いち"], 2 => ["二","に"],
+		3 => ["三","さん"], 4 => ["四","よん"], 5 => ["五","ご"],
+		6 => ["六","ろく"], 7 => ["七","なな"], 8 => ["八","はち"],
+		9 => ["九","きゅう"]
 	}
 
 	BASES = {
-		1    => "", 10 => "十", 100 => "百",
-		1000 => "千", 10000 => "万"
+		1     => ["",""],
+		10    => ["十","じゅう"],
+		100   => ["百","ひゃく"],
+		1000  => ["千","せん"],
+		10000 => ["万","まん"]
 	}
 
-	BASES_TEXT = {
-		1    => "", 10 => "じゅう", 100 => "ひゃく",
-		1000 => "せん", 10000 => "まん"
+	EXCEPTIONS = {
+		300  => [nil,"さんびゃく"],
+		600  => [nil,"ろっぴゃく"],
+		800  => [nil,"はっぴゃく"],
+		3000 => [nil,"さんぜん"],
+		8000 => [nil,"はっせん"]
 	}
-
-	EXCEPTIONS_TEXT = {
-		300  => "さんびゃく", 600  => "ろっぴゃく",
-		800  => "はっぴゃく",
-		3000 => "さんぜん",   8000 => "はっせん"
-	}
-
-  EXCEPTIONS = {}
 
   ZEROES = {
     :text    => "ゼロ",
     :numeral => "０"
   }
 
-  def self.exceptions num, format
-    format == :text ? EXCEPTIONS_TEXT[num] : EXCEPTIONS[num]
+  def self.index_for_format format
+    format == :text ? TEXT_INDEX : NUMERAL_INDEX
+  end
+
+  def self.exceptions num, base, format
+    hash_index = num * base
+    EXCEPTIONS[hash_index][index_for_format(format)] if EXCEPTIONS[hash_index]
   end
 
   def self.numerals num, format
-    format == :text ? NUMERALS_TEXT[num] : NUMERALS[num]
+    NUMERALS[num][index_for_format(format)]
   end
 
   def self.bases num, format
-    format == :text ? BASES_TEXT[num] : BASES[num]
+    BASES[num][index_for_format(format)]
   end
 
   def self.convert_number int, format
-    raise OutOfRangeException.new("Bangou can only process numbers between 0 and 99,999,999") unless in_range?(int)
+    raise OutOfRangeException.new(RANGE_ERROR_MESSAGE) unless in_range?(int)
     return ZEROES[format] if int == 0
+
     digits = int.to_s.split(//).reverse
+
     digits.each_with_index.map do |digit, power|
-      hide_base_from_value = has_more_digits_before_index?(digits, power)
+      hide_base_from_value = has_more_digits_after_index?(digits, power)
       characters_for_digit_in_base(digit.to_i, 10 ** power, format, hide_base_from_value)
     end.reverse.join
   end
 
-  def self.strip_unneeded_characters text
-    text = text[1..-1] if text =~ /^(一).+/
-    text = text[2..-1] if text =~ /^(いち).+/
-    text
+  def self.strip_leading_ones text
+    text.sub(/^(一|いち)(.+)/,'\2')
   end
 
-  def self.has_more_digits_before_index?(digits, index)
+  def self.has_more_digits_after_index?(digits, index)
 		digits.size > 1 and (digits[0..index - 1] & ("1".."9").to_a).size > 0
 	end
 
   def self.characters_for_digit_in_base(num, base, format, hide_base_from_value)
-    if text = exceptions(num * base, format)
+    return "" if num == 0
+
+    if text = exceptions(num, base, format)
       text
+    elsif base <= 10000
+      text = numerals(num, format) + bases(base, format)
     else
-      if num > 0 and base > 10000
-        text = exceptions(num * base/10000, format) || characters_for_digit_in_base(num, base/10000, format, hide_base_from_value)
-        text += bases(10000, format) unless hide_base_from_value
-      elsif num > 0
-        text = numerals(num, format) + bases(base, format)
-      end
-      strip_unneeded_characters(text)
+      text  = characters_for_digit_in_base(num, base/10000, format, hide_base_from_value)
+      text += bases(10000, format) unless hide_base_from_value
     end
+    strip_leading_ones(text)
   end
 end
